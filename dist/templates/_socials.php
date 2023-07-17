0f29c4f9567c7c587c015bd4b6fba68e998af110
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-07-17 11:52:08 +0300

    CMASS-1009

--- dist/templates/_socials.php
+++ dist/templates/_socials.php
@@ -1,16 +1,6 @@
 <div class="socials">
     <a href="#">
         <svg class="icon" width="24" height="24">
-            <use xlink:href="#fb"></use>
-        </svg>
-    </a>
-    <a href="#">
-        <svg class="icon" width="24" height="24">
-            <use xlink:href="#im"></use>
-        </svg>
-    </a>
-    <a href="#">
-        <svg class="icon" width="24" height="24">
             <use xlink:href="#tw"></use>
         </svg>
     </a>
@@ -19,5 +9,16 @@
             <use xlink:href="#ytb"></use>
         </svg>
     </a>
+    <a href="#">
+        <svg class="icon" width="24" height="24">
+            <use xlink:href="#fb"></use>
+        </svg>
+    </a>
+    <a href="#">
+        <svg class="icon" width="24" height="24">
+            <use xlink:href="#im"></use>
+        </svg>
+    </a>
+   
 
 </div>
